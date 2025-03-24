224a5d35a7e44960a4e655b5b18ff5cb406973f3
Tyler Robinson <tjrdesigner@gmail.com>
2025-03-24 11:19:04 -0700

    Refactor Facebook API integration by moving related functions to a separate module and removing redundant code from index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,10 +5,17 @@ import axios from 'axios';
 import admin from 'firebase-admin';
 import { openAiRequest } from './openai';
 import { logLogs, logTime, logs } from './utils';
+import { 
+  facebookGraphRequest, 
+  sendWhatsAppReceipt, 
+  sendMessengerReceipt, 
+  sendMessengerMessage, 
+  sendWhatsAppMessage, 
+  extractWhatsAppMessageDetails 
+} from './facebook';
 
 const openaitoken = process.env.OPENAI_API_KEY ?? '';
 const openAiOrgId = process.env.OPENAI_ORG_ID;
-const pageAccessToken = process.env.PAGE_ACCESS_TOKEN;
 const verifyToken = process.env.VERIFY_TOKEN;
 const notionToken = process.env.NOTION_TOKEN;
 const notionBlockId = process.env.NOTION_BLOCK_ID;
@@ -37,36 +44,6 @@ const currentTime = new Date().toLocaleString('en-US', {
   timeZone: 'America/Los_Angeles',
 });
 
-const facebookGraphRequest = async (
-  endpoint: string,
-  data: any,
-  errorMsg: string,
-  method: string,
-) => {
-  const start = Date.now();
-  try {
-    const response = await axios({
-      method,
-      url: `https://graph.facebook.com/v16.0/${endpoint}access_token=${pageAccessToken}`,
-      data,
-      headers: { 'Content-Type': 'application/json' },
-    });
-    logTime(start, 'sendFBGraphRequest');
-    return response;
-  } catch (error: any) {
-    if (error.response && error.response.data) {
-      const detailedErrorMsg = JSON.stringify(error.response.data);
-      return functions.logger.error(`${errorMsg}: ${detailedErrorMsg}`);
-    } else if (error.request) {
-      // The request was made but no response was received
-      return functions.logger.error(`${errorMsg}: No response received.`);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      return functions.logger.error(`${errorMsg}: ${error.message}`);
-    }
-  }
-};
-
 const getPrimer = async () => {
   const start = Date.now();
   try {
@@ -90,69 +67,6 @@ const getPrimer = async () => {
   } catch (error) {
     functions.logger.error(`Error getting primer: ${error}`);
   }
-};
-
-const sendWhatsAppReceipt = async (phone_number_id: string, msgId: string) => {
-  await facebookGraphRequest(
-    `${phone_number_id}/messages?`,
-    {
-      messaging_product: 'whatsapp',
-      status: 'read',
-      message_id: msgId,
-    },
-    'Error while marking WhatsApp as seen',
-    'POST',
-  );
-};
-
-// Send Messenger receipt
-const sendMessengerReceipt = async (userId: string, sender_action: string) => {
-  await facebookGraphRequest(
-    'me/messages?',
-    {
-      recipient: { id: userId },
-      sender_action,
-    },
-    `Error while sending Messenger action: ${sender_action}`,
-    'POST',
-  );
-};
-
-// Send Messenger message
-const sendMessengerMessage = async (
-  userId: string,
-  response: string,
-  platform: string,
-) => {
-  logLogs(`Sending ${platform} message to ${userId}`);
-  await facebookGraphRequest(
-    'me/messages?',
-    {
-      recipient: { id: userId },
-      message: { text: `${response}` },
-    },
-    `Error while sending ${platform} message`,
-    'POST',
-  );
-};
-
-// Send WhatsApp message
-const sendWhatsAppMessage = async (
-  phoneNumberId: string,
-  userId: string,
-  response: string,
-) => {
-  logLogs('Sending WhatsApp message');
-  await facebookGraphRequest(
-    `${phoneNumberId}/messages?`,
-    {
-      messaging_product: 'whatsapp',
-      to: userId,
-      text: { body: `${response}` },
-    },
-    'Error while sending WhatsApp message',
-    'POST',
-  );
 };
 
 const storeThreadId = async (from: string, thread: any, userName: string) => {
@@ -364,27 +278,6 @@ const getThread = async (from: string, platform: string) => {
     logTime(start, 'getThreadId');
     return;
   }
-};
-
-const extractWhatsAppMessageDetails = (req: {
-  body: { entry: { changes: { value: any }[] }[] };
-}) => {
-  const request = req.body.entry[0].changes[0].value;
-  const message = request.messages[0];
-  const userId = message.from;
-  const messageId = message.id;
-  const msgBody = message.text.body;
-  const name = request.contacts[0]?.profile.name;
-  const phoneNumberId = request.metadata.phone_number_id;
-  const msgId = message.id;
-  return {
-    messageId,
-    userId,
-    msgBody,
-    name,
-    phoneNumberId,
-    msgId,
-  };
 };
 
 const processMessage = async (
