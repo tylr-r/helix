14283e5f55caf3e78302c177ae7a23588199316a
Tyler Robinson <tjrdesigner@gmail.com>
2025-04-10 15:42:24 -0700

    Add main page content to App.tsx

--- public/tylr/src/App.tsx
+++ public/tylr/src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Footer from './Footer';
+import Messenger_logo from './assets/Messenger_logo.svg';
+import WhatsApp_logo from './assets/WhatsApp_logo.svg';
+import Instagram_logo from './assets/Instagram_logo.svg';
+import github_mark from './assets/github-mark.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div id="logo-container">
+        <div id="tyler-text">
+          <span className="left-part">Ty</span>
+          <span id="slash">/</span>
+          <span className="right-part">r</span>
+        </div>
+      </div>
+
+      <div id="main-tiles">
+        <a href="#" id="helix" className="button">
+          <span>helix project</span>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" id="resume" className="button" aria-disabled="true">
+          <span>resumé</span>
+        </a>
+        <a href="#" id="portfolio" className="button" aria-disabled="true">
+          <span>portfolio</span>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div id="chat-links">
+        <a
+          className="social-link"
+          href="https://m.me/tylrcreative"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Messenger_logo} alt="Chat Tylr on Messenger" />
+        </a>
+        <a
+          className="social-link"
+          href="https://wa.me/message/4HK74L3CLAAVE1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={WhatsApp_logo} alt="Chat Tylr on WhatsApp" />
+        </a>
+        <a
+          className="social-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Instagram_logo} alt="Chat Tylr on Instagram" />
+        </a>
+        <a
+          className="social-link"
+          href="https://github.com/tylr-r/helix"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={github_mark}
+            alt="View project on GitHub"
+            className="github-logo"
+          />
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
